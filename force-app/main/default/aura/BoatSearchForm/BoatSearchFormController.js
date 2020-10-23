52c023bbcaeb15daa760b8db9de9bd636fb6e297
Erick Sixto <sixto.hernandez@theksquaregroup.com>
2020-10-23 17:40:57 -0500

    Layout for searchForm

--- force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
+++ force-app/main/default/aura/BoatSearchForm/BoatSearchFormController.js
@@ -1,5 +1,24 @@
 ({
-    myAction : function(component, event, helper) {
+  doInit: function (component) {
+    var action = component.get("c.getBoatTypes");
 
+    var createRecordEvent = $A.get("e.force:createRecord");
+
+    if (createRecordEvent) {
+      component.set("v.isCreateRecordSupported", true);
+    } else {
+      component.set("v.isCreateRecordSupported", false);
     }
-})
+
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+
+      if (state === "SUCCESS") {
+        var result = response.getReturnValue();
+
+        component.set("v.boatTypes", result);
+      }
+    });
+    $A.enqueueAction(action);
+  }
+});
