fe62ff265920140116fb115a7710571d1aec6d71
Erick Sixto <sixto.hernandez@theksquaregroup.com>
2020-10-26 12:27:52 -0600

    Second Challenge Completed

--- force-app/main/default/aura/BoatSearchResults/BoatSearchResultsHelper.js
+++ force-app/main/default/aura/BoatSearchResults/BoatSearchResultsHelper.js
@@ -1,5 +1,22 @@
 ({
-    helperMethod : function() {
+  onSearch: function (component) {
+    var boatTypeId = component.get("v.boatTypeId");
 
-    }
-})
+    var action = component.get("c.getBoats");
+
+    action.setParams({ boatTypeId: boatTypeId });
+
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+
+      if (state === "SUCCESS") {
+        var boats = response.getReturnValue();
+
+        component.set("v.boats", boats);
+      } else {
+        console.log("ERROR");
+      }
+    });
+    $A.enqueueAction(action);
+  }
+});
