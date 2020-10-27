fc5f014fb5a951bb8d2917095092ac5394ea682e
Erick Sixto <sixto.hernandez@theksquaregroup.com>
2020-10-27 16:52:54 -0600

    7th challenge done. AddBoat review

--- force-app/main/default/aura/BoatDetail/BoatDetailController.js
+++ force-app/main/default/aura/BoatDetail/BoatDetailController.js
@@ -1,5 +1,21 @@
 ({
-    myAction : function(component, event, helper) {
+  init: function (component, event, helper) {
+    var redirectEvent = $A.get("e.force:navigateToSObject");
 
+    if (redirectEvent) {
+      component.set("v.showFullDetailsBtn", true);
     }
-})
+  },
+
+  onFullDetails: function (component, event, helper) {
+    var boat = component.get("v.boat");
+    var redirectEvent = $A.get("e.force:navigateToSObject");
+
+    if (redirectEvent && boat) {
+      redirectEvent.setParams({
+        recordId: boat.Id
+      });
+      redirectEvent.fire();
+    }
+  }
+});
--- force-app/main/default/aura/BoatDetails/BoatDetailsController.js
+++ force-app/main/default/aura/BoatDetails/BoatDetailsController.js
@@ -6,5 +6,15 @@
 
     component.find("service").reloadRecord();
   },
-  onRecordUpdated: function (component, event) {}
+  onRecordUpdated: function (component, event) {},
+
+  onBoatReviewAdded: function (component, event, helper) {
+    var reviews = component.find("reviews");
+
+    if (reviews) {
+      reviews.refresh();
+    }
+
+    component.find("tabs").set("v.selectedTabId", "boatreviewtab");
+  }
 });
